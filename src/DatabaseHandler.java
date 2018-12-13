6387c0c52d55b5434372a4de8e9b84f48bc3eecc
Ely Merenstein <elysamuel16@gmail.com>
2018-12-13 16:59:13 -0700

    add rides to database, get a list of rides from database

---
+++ src/DatabaseHandler.java
@@ -0,0 +1,93 @@
+import java.sql.*;
+import java.util.ArrayList;
+
+public class DatabaseHandler {
+	
+	public static final String PORT_NUMBER = "8889";
+	
+	private static Statement databaseStatement; // Does this need to be closed ever?
+	
+	public static void initialize() {
+		try(
+				Connection initialConn = DriverManager.getConnection("jdbc:mysql://localhost:" + PORT_NUMBER + "/", 
+						"root", "root");
+				Statement initialStmt = initialConn.createStatement();
+				){
+			
+			String createDatabaseString = "create database if not exists CCRideShare";
+			initialStmt.execute(createDatabaseString);
+		
+			Connection conn = DriverManager.getConnection(
+				"jdbc:mysql://localhost:" + PORT_NUMBER + "/CCRideShare?user=root&password=root");
+			databaseStatement = conn.createStatement();	
+		
+			String createTable = "create table if not exists RidePosts ( " + 
+					 "id int not null auto_increment, "
+					 + "date varchar(25), "
+					 + "time varchar(10), "
+					 + "toLocation varchar(50), "
+					 + "fromLocation varchar(50), "
+					 + "numSpots int, "
+					 + "price varchar(50), "
+					 + "comments varchar(500), "
+					 + "primary key (id));";
+			
+			databaseStatement.execute(createTable);
+			
+		}catch(SQLException ex) {
+			ex.printStackTrace();
+		}
+	}
+	
+	/*public static Statement getStatement() {	
+		try {
+				// Set up new Connection and Statement, now that the database is created
+				Connection conn = DriverManager.getConnection(
+						"jdbc:mysql://localhost:" + PORT_NUMBER + "/CCRideShare?user=root&password=root");
+				Statement stmt = conn.createStatement();
+			return stmt;
+		}catch(SQLException ex) {
+			ex.printStackTrace();
+			return null;
+		}
+		
+	}*/
+	
+	public static void addRidePost(RidePost ridePost) {
+		String sqlInsert = String.format("INSERT INTO RidePosts values (null, '%s', '%s', '%s', '%s', %d, '%s', '%s')", 
+				 ridePost.getDate(), ridePost.getTime(), ridePost.getToLocation(), ridePost.getFromLocation(), ridePost.getNumSpots(), ridePost.getPrice(), ridePost.getComments());
+
+		try{
+			databaseStatement.executeUpdate(sqlInsert);
+		}catch(SQLException ex) {
+			ex.printStackTrace();
+		}
+	}
+	
+	public static ArrayList<RidePost> getRidePosts() {
+		String sqlSelect = "select * from RidePosts";
+		try{
+			ResultSet rset = databaseStatement.executeQuery(sqlSelect);
+			ArrayList<RidePost> ridePosts = new ArrayList<RidePost>();
+			while(rset.next()) {
+				String date = rset.getString("date");
+				String time = rset.getString("time");
+				String toLocation = rset.getString("toLocation");
+				String fromLocation = rset.getString("fromLocation");
+				int numSpots = rset.getInt("numSpots");
+				String price = rset.getString("price");
+				String comments = rset.getString("comments");
+				RidePost currRidePost = new RidePost(date, time, toLocation, fromLocation, numSpots, price, comments);
+				
+				ridePosts.add(currRidePost);
+			}
+			
+			rset.close();
+			return ridePosts;
+		}catch(SQLException ex) {
+			ex.printStackTrace();
+			System.exit(-1);
+			return null;
+		}
+	}
+}
--- src/RidePost.java
+++ src/RidePost.java
@@ -1,5 +1,6 @@
+import java.util.ArrayList;
 
-public class Ride {
+public class RidePost {
 
 	private String date;
 	private String time;
@@ -9,8 +10,18 @@ public class Ride {
 	private String price;
 	private String comments;
 	
-	public Ride() {
-		
+	// private User driver;
+	// private ArrayList<User> riders;
+	
+	public RidePost(String date, String time, String toLocation, String fromLocation, int numSpots, String price, String comments) {
+		this.date = date; 
+		this.time = time;
+		this.toLocation = toLocation;
+		this.fromLocation = fromLocation;
+		this.numSpots = numSpots;
+		this.price = price;
+		this.comments = comments;
+		// databaseStatement = DatabaseHandler.getStatement();
 	}
 
 	public String getDate() {
@@ -69,6 +80,16 @@ public class Ride {
 		this.comments = comments;
 	}
 	
+	public void addToDatabase() {
+		DatabaseHandler.addRidePost(this);
+	}
 	
-	
+	public static void main(String[] args) {
+		DatabaseHandler.initialize();
+		RidePost post = new RidePost("1/1/19", "11:00PM", "Dover", "Colorado College", 4, "$20", "no comments");
+		RidePost post2 = new RidePost("1/1/20", "11:00PM", "Denver", "Colorado College", 4, "$20", "");
+		post.addToDatabase();
+		post2.addToDatabase();
+		ArrayList<RidePost> ridePosts = DatabaseHandler.getRidePosts();
+	}
 }
