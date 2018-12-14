8b12e11980a29e492849e2be65be38b187c966f4
viktorattilakelemen <43358345+viktorattilakelemen@users.noreply.github.com>
2018-12-13 21:45:38 -0700

    GUI fix
    Quick fix, main.java works now

--- src/application/Main.java
+++ src/application/Main.java
@@ -12,7 +12,7 @@ public class Main extends Application {
 	@Override
 	public void start(Stage primaryStage) {
 		try {
-			Parent root = FXMLLoader.load(getClass().getResource("/ui/Test.fxml"));
+			Parent root = FXMLLoader.load(getClass().getResource("/ui/RideListGUI.fxml"));
 			Scene scene = new Scene(root);
 //			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			primaryStage.setScene(scene);
