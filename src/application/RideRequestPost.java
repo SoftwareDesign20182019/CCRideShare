095bc5198df4d16aeec498e16e3eedeffd6d221f
Alex-Rehorst <45635927+Alex-Rehorst@users.noreply.github.com>
2018-12-14 16:42:37 -0700

    Added new ride request post database, added ability to view request database contents in 'ride request' tab in RideListGUI.

---
+++ src/application/RideRequestPost.java
@@ -0,0 +1,88 @@
+package application;
+import java.util.ArrayList;
+
+
+/**
+* Class for ride request posts - a post listed by riders for drivers looking for riders
+* @author arehorst
+*
+*/
+public class RideRequestPost {
+
+	private String date;
+	private String time;
+	private String toLocation;
+	private String fromLocation;
+	private String comments;
+
+	public RideRequestPost(String date, String time, String toLocation, String fromLocation, String comments) {
+		this.date = date; 
+		this.time = time;
+		this.toLocation = toLocation;
+		this.fromLocation = fromLocation;
+		this.comments = comments;
+	}
+	
+	public String getDate() {
+		return date;
+	}
+
+	public void setDate(String date) {
+		this.date = date;
+	}
+
+	public String getTime() {
+		return time;
+	}
+
+	public void setTime(String time) {
+		this.time = time;
+	}
+
+	public String getToLocation() {
+		return toLocation;
+	}
+
+	public void setToLocation(String toLocation) {
+		this.toLocation = toLocation;
+	}
+
+	public String getFromLocation() {
+		return fromLocation;
+	}
+
+	public void setFromLocation(String fromLocation) {
+		this.fromLocation = fromLocation;
+	}
+
+	public String getComments() {
+		return comments;
+	}
+
+	public void setComments(String comments) {
+		this.comments = comments;
+	}
+	
+	/**
+	 * adds a RiderPost to the database
+	 * @return number of rows added
+	 */
+	public int addToDatabase() {
+		int rowsAdded = DatabaseHandler.addRideRequestPost(this);
+		return rowsAdded;
+	}
+	
+	/**
+	 * main method
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		
+		DatabaseHandler.initialize();
+		RideRequestPost post = new RideRequestPost("10/5/19", "9:00AM", "Hell", "and Back", "no comments");
+		RideRequestPost post2 = new RideRequestPost("12/7/20", "3:45PM", "DIA", "Colorado College", "");
+		post.addToDatabase();
+		post2.addToDatabase();
+		ArrayList<RideRequestPost> riderequestPosts = DatabaseHandler.getRideRequestPosts();		
+	}
+}
--- src/ui/RideShareGUIController.java
+++ src/ui/RideShareGUIController.java
@@ -8,6 +8,7 @@ import java.util.ResourceBundle;
 
 import application.DatabaseHandler;
 import application.RidePost;
+import application.RideRequestPost;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
@@ -18,10 +19,10 @@ import javafx.scene.control.TableView;
 import java.util.ArrayList;
 /**
  * Connects the .fxml GUI file with the backend oprations
- * @author viktorkelemen & elymerenstein
+ * @author viktorkelemen & elymerenstein & arehorst
  */
 public class RideShareGUIController{
-
+// Ride Tab
 	@FXML
 	private URL location;
 	@FXML
@@ -37,46 +38,75 @@ public class RideShareGUIController{
 	@FXML
 	private TableColumn avail_Seats_col;
 	@FXML
-	ObservableList<RidePost> data;
-	
-	
+	ObservableList<RidePost> Ridedata;
+// Ride Request Tab
+	@FXML
+	private TableView<RideRequestPost> riderequestpost_table;
+	@FXML
+	private TableColumn request_time_col;
+	@FXML
+	private TableColumn request_to_col;
+	@FXML
+	private TableColumn request_from_col;
+	@FXML
+	ObservableList<RideRequestPost> Requestdata;
+
 	public RideShareGUIController() {
+		//Ride Tab
 		application.Main.primaryStage.setResizable(false);
 		ridepost_table = new TableView<RidePost>();
 		time_col = new TableColumn();
 		to_col = new TableColumn();
 		from_col = new TableColumn();
 		avail_Seats_col = new TableColumn();
-		data = FXCollections.observableArrayList();
+		Ridedata = FXCollections.observableArrayList();
+		//Ride Request Tab
+		riderequestpost_table = new TableView<RideRequestPost>();
+		request_time_col = new TableColumn();
+		request_to_col = new TableColumn();
+		request_from_col = new TableColumn();
+		Requestdata = FXCollections.observableArrayList();
 	}
-	
+
 	/**
 	 * Method to be called automatically when this controller is attached to the FXML file
 	 */
 	@FXML
 	private void initialize() {
-		
+		// Ride Tab
 		time_col.setCellValueFactory(new PropertyValueFactory<RidePost, String>("time"));
-		
+
 		to_col.setCellValueFactory(new PropertyValueFactory<RidePost, String>("toLocation"));
-		
+
 		from_col.setCellValueFactory(new PropertyValueFactory<RidePost, String>("fromLocation"));
-		
+
 		avail_Seats_col.setCellValueFactory(new PropertyValueFactory<RidePost, String>("numSpots"));
-		
-		ridepost_table.setItems(data);
-		
+
+		ridepost_table.setItems(Ridedata);
+
 		ArrayList<RidePost> ridePosts = DatabaseHandler.getRidePosts();
-		data.addAll(ridePosts);
+		Ridedata.addAll(ridePosts);
+
+		//Ride Request Tab
+		request_time_col.setCellValueFactory(new PropertyValueFactory<RidePost, String>("time"));
+
+		request_to_col.setCellValueFactory(new PropertyValueFactory<RidePost, String>("toLocation"));
+
+		request_from_col.setCellValueFactory(new PropertyValueFactory<RidePost, String>("fromLocation"));
+
+		riderequestpost_table.setItems(Requestdata);
+
+		ArrayList<RideRequestPost> riderequestPosts = DatabaseHandler.getRideRequestPosts();
+		Requestdata.addAll(riderequestPosts);
 	}
-	
-//	public void addToRidePostList(RidePost ridePost) {
-//		data.add(ridePost);
-//	}
-//	
-//	public void addAllToRidePostList(ArrayList<RidePost> ridePosts) {
-//		data.addAll(ridePosts);
-//	}
-	
-	
+
+	//	public void addToRidePostList(RidePost ridePost) {
+	//		data.add(ridePost);
+	//	}
+	//	
+	//	public void addAllToRidePostList(ArrayList<RidePost> ridePosts) {
+	//		data.addAll(ridePosts);
+	//	}
+
+
 }
